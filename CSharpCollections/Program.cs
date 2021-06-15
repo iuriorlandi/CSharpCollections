d0860df7324be98243f6c4ea53d5243a4714d327
Iuri <iuri.orlandi20@gmail.com>
2021-06-15 20:01:21 -0300

    Estudando arrays.

--- CSharpCollections/Program.cs
+++ CSharpCollections/Program.cs
@@ -6,7 +6,30 @@ namespace CSharpCollections
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] aulas = new string[3];
+
+            var aula1 = "Introdução.";
+            var aula2 = "Conteúdo.";
+            var aula3 = "Exercícios.";
+
+            aulas[0] = aula1;
+            aulas[1] = aula2;
+            aulas[2] = aula3;
+
+            string[] copiaComTamanhoDois = new string[2];
+
+            Array.Copy(aulas, 0, copiaComTamanhoDois, 0, 2);
+
+            foreach (var aula in aulas)
+            {
+                Console.WriteLine(aula);
+            }
+
+            foreach (var aula in copiaComTamanhoDois)
+            {
+                Console.WriteLine(aula);
+            }
+
         }
     }
 }
