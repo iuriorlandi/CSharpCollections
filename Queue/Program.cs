93f1b39f3f3afe209c0af6b134d81dba562d1358
Iuri <iuri.orlandi20@gmail.com>
2021-06-26 15:50:50 -0300

    Trabalhando com Queue

---
+++ Queue/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> pedagio = new();
+
+            Enfileirar("Carro", pedagio);
+            Enfileirar("Caminhão", pedagio);
+            Enfileirar("Onibus", pedagio);
+            Enfileirar("Carreta", pedagio);
+
+            Desenfileirar(pedagio);
+            Desenfileirar(pedagio);
+            Desenfileirar(pedagio);
+            Desenfileirar(pedagio);
+
+        }
+
+        private static void Desenfileirar(Queue<string> pedagio)
+        {
+            if (!pedagio.Any())
+                return;
+
+            var veiculo = pedagio.Dequeue();
+            
+            Console.WriteLine();
+            Console.WriteLine($"{veiculo} saiu da fila!");
+
+            ImprimirFila(pedagio);
+        }
+
+        private static void Enfileirar(string veiculo, Queue<string> pedagio)
+        {
+            pedagio.Enqueue(veiculo);
+
+            Console.WriteLine();
+            Console.WriteLine($"{veiculo} entrou na fila!");
+            
+            ImprimirFila(pedagio);
+        }
+
+        private static void ImprimirFila(Queue<string> pedagio)
+        {
+            
+            Console.WriteLine("Fila:");
+            foreach (var veiculo in pedagio)
+            {
+                Console.WriteLine(veiculo);
+            }
+        }
+    }
+}
