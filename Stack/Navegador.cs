bd5f927e1915c43dde15d1fe9e4955d12d3958a4
Iuri <iuri.orlandi20@gmail.com>
2021-06-26 15:21:42 -0300

    Trabalhando com Stack

---
+++ Stack/Navegador.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Stack
+{
+    internal class Navegador
+    {
+        private readonly Stack <string> _historicoAnterior = new();
+        private readonly Stack<string> _historicoPosterior = new();
+        private string _paginaAtual;
+
+        public Navegador()
+        {
+            _paginaAtual = "HomePage";
+            ImprimirPaginaAtual();
+        }
+
+        internal void NavegarPara(string url)
+        {
+            _historicoAnterior.Push(_paginaAtual); 
+            _paginaAtual = url;
+            ImprimirPaginaAtual();
+        }
+
+        internal void Proxima()
+        {
+            NavegarHistorico(_historicoAnterior, _historicoPosterior);
+        }
+
+        private void NavegarHistorico(Stack<string> pilhaOrigem, Stack<string> pilhaDestino)
+        {
+            if (!pilhaDestino.Any())
+                return;
+
+            pilhaOrigem.Push(_paginaAtual);
+            _paginaAtual = pilhaDestino.Pop();
+            ImprimirPaginaAtual();
+
+        }
+
+        internal void Anterior()
+        {
+            NavegarHistorico(_historicoPosterior, _historicoAnterior);
+        }
+
+        private void ImprimirPaginaAtual()
+        {
+            Console.WriteLine(_paginaAtual);
+        }
+    }
+}
---
+++ Stack/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var navegador = new Navegador();
+
+            navegador.NavegarPara("google.com");
+            navegador.NavegarPara("alura.com");
+            navegador.NavegarPara("youtube.com");
+
+            navegador.Anterior();
+            navegador.Anterior();
+            navegador.Anterior();
+
+            navegador.Proxima();
+            navegador.Proxima();
+
+            navegador.Anterior();
+            navegador.Anterior();
+            navegador.Anterior();
+        }
+    }
+}
