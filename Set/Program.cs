011b97b339ffa9a4660c9a5c106001931a30d106
Iuri <iuri.orlandi20@gmail.com>
2021-07-03 11:04:43 -0300

    estudando Sets

---
+++ Set/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Set
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISet<string> alunos = new HashSet<string>();
+
+            alunos.Add("João");
+            alunos.Add("Bianca");
+            alunos.Add("Carlos");
+
+            Console.WriteLine(string.Join(",", alunos));
+
+            alunos.Add("Patricia");
+            alunos.Add("Luiz");
+            alunos.Add("Pedro");
+
+            Console.WriteLine(string.Join(",", alunos));
+        }
+    }
+}
